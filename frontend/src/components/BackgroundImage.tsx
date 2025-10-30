e5062be825cb3ecf51ba6d24ac972b55c5d87acd
Marina Glen Admin <admin@marinaglen.co.za>
2025-10-30 17:32:41 +0200

    Aggressive multi-method background approach - CSS pseudo-elements, img tags, and public folder

--- frontend/src/components/BackgroundImage.tsx
+++ frontend/src/components/BackgroundImage.tsx
@@ -2,22 +2,64 @@ import React from 'react'
 import marinaBackground from '../assets/marina-beach-ocean-bg.jpg'
 
 const BackgroundImage: React.FC = () => {
-  const backgroundStyle: React.CSSProperties = {
+  const containerStyle: React.CSSProperties = {
     position: 'fixed',
     top: 0,
     left: 0,
     width: '100vw',
     height: '100vh',
-    backgroundImage: `url(${marinaBackground})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center center',
-    backgroundRepeat: 'no-repeat',
-    backgroundAttachment: 'fixed',
     zIndex: -1000,
     backgroundColor: '#0891b2'
   }
 
-  return <div style={backgroundStyle} className="background-image-component" />
+  const imageStyle: React.CSSProperties = {
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+    objectPosition: 'center center'
+  }
+
+  const cssBackgroundStyle: React.CSSProperties = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100vw',
+    height: '100vh',
+    backgroundImage: `url(/background.jpg)`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center center',
+    backgroundRepeat: 'no-repeat',
+    backgroundAttachment: 'fixed',
+    zIndex: -1001,
+    backgroundColor: '#0891b2'
+  }
+
+  return (
+    <>
+      {/* Method 1: CSS Background from public folder */}
+      <div style={cssBackgroundStyle} className="bg-css-public" />
+      
+      {/* Method 2: IMG tag with imported asset */}
+      <div style={containerStyle} className="bg-img-container">
+        <img 
+          src={marinaBackground} 
+          alt="Marina Glen Ocean Background" 
+          style={imageStyle}
+          className="bg-img-asset"
+        />
+      </div>
+      
+      {/* Method 3: IMG tag from public folder */}
+      <div style={{...containerStyle, zIndex: -1002}} className="bg-img-public-container">
+        <img 
+          src="/background.jpg" 
+          alt="Marina Glen Ocean Background" 
+          style={imageStyle}
+          className="bg-img-public"
+        />
+      </div>
+    </>
+  )
 }
 
 export default BackgroundImage
